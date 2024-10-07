d1a2f17291d2aacfce6493c03cd04d810861c0db
buiducviettt <ducviet8a4@gmail.com>
2024-10-07 20:54:55 +0700

    done detail

--- src/assets/image/Images.jsx
+++ src/assets/image/Images.jsx
@@ -5,6 +5,9 @@ import styleitem1 from './DressStyleCate/styleitem1.jpg'
 import styleitem2 from './DressStyleCate/styleitem2.jpg'
 import styleitem3 from './DressStyleCate/styleitem3.jpg'
 import styleitem4 from './DressStyleCate/styleitem4.jpg'
+import styleitem5 from './DressStyleCate/styleitem5.jpg'
+import styleitem6 from './DressStyleCate/styleitem6.jpg'
+import styleitem7 from './DressStyleCate/styleitem7.jpg'
 const Image = {
     logo,
     bannerHome,
@@ -12,7 +15,10 @@ const Image = {
     styleitem1,
     styleitem2,
     styleitem3,
-    styleitem4
+    styleitem4,
+    styleitem5,
+    styleitem6,
+    styleitem7
     
     
 } 
--- src/components/Categories/ProductDetails/index.jsx
+++ src/components/Categories/ProductDetails/index.jsx
@@ -3,10 +3,12 @@ import { useParams} from "react-router-dom";
 import DefaultLayout from '../../Layout/Default Layout/index'
 import styles from './productdetail.module.scss'
 import Button from "../../Button";
+import Image from "../../../assets/image/Images";
 const ProductDetail = () => {
     const { productId } = useParams();
     const [product, setProduct] = useState('');
     const [loading, setLoading] = useState(true);
+     const [selectedImage, setSelectedImage] = useState('');
     // goi API 
     useEffect(() => {
         const fetchAPI = async () => {
@@ -14,6 +16,7 @@ const ProductDetail = () => {
                 const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
                 const data = await response.json();
                 setProduct(data);
+                setSelectedImage(data.image);
             }
             catch (error) {
                 console.error(`Error from ${error}`)
@@ -34,8 +37,42 @@ const ProductDetail = () => {
                     <div className="row">
                         <div className={`col col-md-6 `}>
                             <div className={styles.wrapper}>
+                                <div className="row">
+                                    <div className="col col-md-4">
+                                        <div className={styles.gallery}>
+                                            <img 
+                                            src={product.image} 
+                                            alt="" 
+                                            className={styles.selectedImage} 
+                                            onClick={() => setSelectedImage(product.image)}
+                                            />
+                                            <img 
+                                            src={Image.styleitem5} 
+                                            alt="" 
+                                            className={styles.thumbnail} 
+                                            onClick={() => setSelectedImage(Image.styleitem5)}
+                                            />
+                                             <img 
+                                            src={Image.styleitem6} 
+                                            alt="" 
+                                            className={styles.thumbnail} 
+                                            onClick={() => setSelectedImage(Image.styleitem6)}
+                                            />
+                                             <img 
+                                            src={Image.styleitem7} 
+                                            alt="" 
+                                            className={styles.thumbnail} 
+                                            onClick={() => setSelectedImage(Image.styleitem7)}
+                                        />
+                                            </div>
+
+                                        </div>
+                                    <div className="col col-md-8">
                             <div className={styles.productImage}>
-                                <img src={product.image} alt="" />
+                                            <img src={selectedImage} alt="" />
+                                        </div>
+                                        
+                                </div>
                                 </div>
                                 </div>
                         </div>
