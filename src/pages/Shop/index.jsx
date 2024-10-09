653668bb78e7148ecc901232bc78cfffba32c62d
buiducviettt <ducviet8a4@gmail.com>
2024-10-09 16:57:05 +0700

    done filter

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,8 +1,10 @@
 import { useEffect ,useState} from "react";
 import ProductList from "../../components/Categories/Product List"
 import DefaultLayout from "../../components/Layout/Default Layout";
+import styles from './shop.module.scss';
 const Shop = () => {
     const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("men's clothing");
     useEffect(() => {
         const fetchCategories = async () => { 
             try {
@@ -20,15 +22,20 @@ const Shop = () => {
         <DefaultLayout>
             <div className="container mt-5">
                 <div className="row">
-                    <div className="col col-md-4">
+                    <div className={`col col-md-4 ${styles.filterWrapper}`}>
                         <h1>Filter</h1>
+                        {categories.map((category) => (
+                            <li style={{cursor:"pointer"}} key={category } onClick={()=>{setSelectedCategory(category)}} >{category}</li>     
+                        ))}
                     </div>
                 <div className="col col-md-8">
-                <h1>Casual</h1>
-            {categories.map((category) => {
-               return <ProductList key={category} category={category} />
+                        <h1>{selectedCategory || "All Product"}</h1>
+                        <ProductList
+                    key={selectedCategory}
+                            category={selectedCategory}
+                              />  
+                            
                 
-            })}
                         </div>
                     </div>
             
