ad0918ac314fc906ef178164dcf5e287497158d1
buiducviettt <ducviet8a4@gmail.com>
2024-10-13 17:39:31 +0700

    Hoan thien cart

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import GlobalStyle from './components/GlobalStyles/index.jsx';
 import { CartProvider } from './pages/Cart/CartContext/index.jsx';
 
 import 'react-loading-skeleton/dist/skeleton.css';
-import { CartContext } from './pages/Cart/CartContext/index.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,11 +14,5 @@ createRoot(document.getElementById('root')).render(
         <App />
       </GlobalStyle>
     </CartProvider>
-
-    <CartContext>
-      <GlobalStyle>
-        <App />
-      </GlobalStyle>
-    </CartContext>
   </StrictMode>,
 );
