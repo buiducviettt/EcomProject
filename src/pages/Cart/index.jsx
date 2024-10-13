4a883452035cdf56e978c639ba352f8616c54d51
buiducviettt <ducviet8a4@gmail.com>
2024-10-13 17:58:21 +0700

    done

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,10 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { CartContext } from './CartContext';
 import DefaultLayout from '../../components/Layout/Default Layout';
 import styles from './cart.module.scss';
 
 const Cart = () => {
   const { cart, removeProduct } = useContext(CartContext);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    const calculatedTotal = cart.reduce((accumulator, item) => {
+      return accumulator + item.price * item.quantity;
+    }, 0);
+    const roundedTotal = Math.round(calculatedTotal * 10) / 10;
+    setTotal(roundedTotal);
+  }, [cart]);
 
   if (cart.length === 0) {
     return (
@@ -18,9 +26,9 @@ const Cart = () => {
   return (
     <DefaultLayout>
       <div className="container">
-        <h1>Your Cart</h1>
         <div className="row">
-          <div className="col col-md-6">
+          <div className="col col-md-7">
+            <h1>Your Cart</h1>
             <div className={styles.cartList}>
               {cart.map((item) => (
                 <div key={item.id} className={styles.cartItem}>
@@ -42,6 +50,23 @@ const Cart = () => {
                 </div>
               ))}
             </div>
+          </div>
+          <div className="col col-md-5">
+            <h1>Order Summary</h1>
+            <table className={styles.tableInfo}>
+              <tr>
+                <div className={styles.tableRow}>
+                  <th>Subtotal</th>
+                  <td>{total}</td>
+                </div>
+              </tr>
+              <tr>
+                <div className={styles.tableRow}>
+                  <th>Total</th>
+                  <td>{total}</td>
+                </div>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
